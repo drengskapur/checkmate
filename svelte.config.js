cd87fbe51b0932eb6f8459e758a343fb22d9421b
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2024-09-30 12:17:03 -0400

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,30 @@
-import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter()
-  }
-}
 
-export default config
+  kit: {
+    adapter: adapter(),
+
+    // Options for the static adapter
+    // See https://github.com/sveltejs/kit/tree/master/packages/adapter-static#options
+    files: {
+      assets: 'static',
+      lib: 'src/lib',
+      routes: 'src/routes',
+      template: 'src/app.html'
+    },
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/checkmate' : ''
+    },
+    // Prerendering options
+    // See https://kit.svelte.dev/docs/page-options#prerender
+    prerender: {
+      default: true
+    }
+  }
+};
+
+export default config;
